8fc47cd14fc402cddcb9e98007986753b939624c
Lukylix <lucas.garcia.bobi@gmail.com>
2021-06-23 21:44:01 +0200

    Done basic function

--- database.js
+++ database.js
@@ -5,9 +5,29 @@ let { Schema, model } = mongoose;
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const UrlSchema = new Schema({
-  original_url: { type: String, required: true, unique: true },
-  short_url: { type: String, required: true },
+  original_url: { type: String, required: true },
+  short_url: { type: String, required: true, unique: true },
 });
 
+const Url = model("Url", UrlSchema);
+
+const createOneUrl = (urlData, done) => {
+  let entry = new Url(urlData);
+  entry.save((err, data) => {
+    if (err) return done(err);
+    done(null, data);
+  });
+};
+
+const findUrlByShort = (short_url) => {
+  return Url.findOne({ short_url: short_url })
+    .then((data) => {
+      return data;
+    })
+    .catch((e) => {throw e});
+};
+
 exports.UrlSchema = UrlSchema;
-exports.UrlModel = model("UrlModel", UrlSchema);
+exports.UrlModel = Url;
+exports.findUrlByShort = findUrlByShort;
+exports.createOneUrl = createOneUrl;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 const crypto = require("crypto");
 
-const UrlModel = require("./database").UrlModel;
+const { createOneUrl, findUrlByShort } = require("./database");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -26,15 +26,36 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
-app.post("/api/shorturl", (req, res) => {
-  let short_url = randomShortUrl();
+app.post("/api/shorturl", async (req, res, next) => {
+  if (
+    !req.body.url.match(
+      /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/
+    )
+  )
+    return res.json({ error: "Invalid URL" });
 
-  let url = new UrlModel({ original_url: req.body.url, short_url });
-  res.json({ original_url: req.body.url, short_url });
+  let short_url = await randomUniqShortUrl();
+
+  createOneUrl({ original_url: req.body.url, short_url }, (err, data) => {
+    if (err) return next(err);
+    if (!data) return next({ message: "Missing callback argument" });
+    res.json({ original_url: data.original_url, short_url: data.short_url });
+  });
 });
 
+app.get("/api/shorturl/:shorturl", async (req, res, next) => {
+  try {
+    let data = await findUrlByShort(req.params.shorturl);
+    res.redirect(data.original_url);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//Error handler
 app.use(function (err, req, res, next) {
   if (err) {
+    console.log(err);
     res
       .status(err.status || 500)
       .type("txt")
@@ -46,16 +67,20 @@ app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
 
+async function randomUniqShortUrl() {
+  let short_url = randomShortUrl();
+  const data = await findUrlByShort(short_url);
+  return data ? randomUniqShortUrl() : short_url;
+}
+
 function randomShortUrl(length = 4) {
   let short_url = crypto.randomInt(Math.pow(36, length)).toString(36);
 
-  let uppercasePatch = crypto.randomInt(Math.pow(2, length)).toString(2).split("");
+  let uppercaseMask = crypto.randomInt(Math.pow(2, length)).toString(2).split("");
   short_url = short_url.split("");
-  for (let x = 0; x < short_url.length; x++) {
-    if (uppercasePatch[x] == "1") short_url[x] = short_url[x].toUpperCase();
-  }
-
+  for (let x = 0; x < short_url.length; x++) if (uppercaseMask[x] == "1") short_url[x] = short_url[x].toUpperCase();
   short_url = short_url.join("");
+
   if (short_url.length < length) short_url = randomShortUrl();
   return short_url;
 }
